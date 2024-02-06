a54a7a6a60f2cb29b5bfb080a1036845705a0e60
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-02-06 12:36:13 +0530

    chore: update project logo

--- src/manifest.js
+++ src/manifest.js
@@ -1,5 +1,5 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import packageData from '../package.json' assert { type: 'json' }
+import { defineManifest } from "@crxjs/vite-plugin";
+import packageData from "../package.json" assert { type: "json" };
 
 export default defineManifest({
   name: packageData.name,
@@ -7,38 +7,43 @@ export default defineManifest({
   version: packageData.version,
   manifest_version: 3,
   icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
+    16: "img/logo.png",
+    32: "img/logo.png",
+    48: "img/logo.png",
+    128: "img/logo.png",
   },
   action: {
-    default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_popup: "popup.html",
+    default_icon: "img/logo.png",
   },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
+  options_page: "options.html",
+  devtools_page: "devtools.html",
   background: {
-    service_worker: 'src/background/index.js',
-    type: 'module',
+    service_worker: "src/background/index.js",
+    type: "module",
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/contentScript/index.js'],
+      matches: ["http://*/*", "https://*/*"],
+      js: ["src/contentScript/index.js"],
     },
   ],
   side_panel: {
-    default_path: 'sidepanel.html',
+    default_path: "sidepanel.html",
   },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: [
+        "img/logo.png",
+        "img/logo.png",
+        "img/logo.png",
+        "img/logo.png",
+      ],
       matches: [],
     },
   ],
-  permissions: ['sidePanel', 'storage'],
+  permissions: ["sidePanel", "storage"],
   chrome_url_overrides: {
-    newtab: 'newtab.html',
+    newtab: "newtab.html",
   },
-})
+});
